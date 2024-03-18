5fa0107dbcef6dd80bcb943e9583b88ee586275a
toneillbroad <toneill@broadinstitute.org>
2024-03-18 15:54:08 -0400

    Use replace instead of create in gcloud run jobs

--- misc/bin/deploy-job.sh
+++ misc/bin/deploy-job.sh
@@ -53,7 +53,7 @@ gcloud builds submit \
 ################################################################
 # Deploy job
 
-gcloud run jobs create $instance_name \
+gcloud run jobs replace $instance_name \
     --cpu=1 \
     --image=$image \
     --max-retries=0 \
